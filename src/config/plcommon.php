1fa147b18e1ffb978ee88578fc79b6d9aa994f23
ericsh <1104534940@qq.com>
2019-05-16 11:11:53 +0800

    是否发生邮件配置化

--- src/config/plcommon.php
+++ src/config/plcommon.php
@@ -12,4 +12,9 @@ return [
      */
     'name' => env('APP_NAME','pl\common'),
 
+    /**
+     * 是否发生邮件 true是 false否
+     */
+    'switch' => env('PL_COMMON_SWITCH',true),
+
 ];
--- src/lib/MailCommon.php
+++ src/lib/MailCommon.php
@@ -26,19 +26,22 @@ class MailCommon
      */
     public function post_email($data,$subject = '报错信息',$view_str = 'vendor.common.emailError',$to = null,$from = null,$from_name = null)
     {
-        // 收件人邮箱
-        $to = $to == null?config('plcommon.username'):$to; // 收件人邮箱
-        $from = $from == null?config('plcommon.username'):$from; // 发件人邮箱
-        $from_name = $from_name == null?config('plcommon.name'):$from_name; // 发件人名称
-        // 邮件发送
-        $num = Mail::send($view_str,['data' => $data],function($message) use($subject,$to,$from,$from_name){
-            $message->from($from, $from_name);
-            $message->subject($subject);
-            $message->to($to);
-        });
+        if(config('plcommon.switch'))
+        {
+            // 收件人邮箱
+            $to = $to == null?config('plcommon.username'):$to; // 收件人邮箱
+            $from = $from == null?config('plcommon.username'):$from; // 发件人邮箱
+            $from_name = $from_name == null?config('plcommon.name'):$from_name; // 发件人名称
+            // 邮件发送
+            $num = Mail::send($view_str,['data' => $data],function($message) use($subject,$to,$from,$from_name){
+                $message->from($from, $from_name);
+                $message->subject($subject);
+                $message->to($to);
+            });
 
-        Log::info('邮件发送返回:'.$num);
+            Log::info('邮件发送返回:'.$num);
 
-        return $num;
+            return $num;
+        }
     }
 }
